9c450289e322bfaa7ef70aa09c0cd61f755e52d9
Lucas Bezerra da Cruz <lucasbezerracrz@gmail.com>
2024-10-22 08:38:42 -0300

    Second Commit

--- lib/login/tela1.dart
+++ lib/login/tela1.dart
@@ -1,16 +1,54 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
-  final String title;
-  const Login({super.key, required this.title});
+  const Login({super.key});
 
   @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: 100,
-      color: Colors.blue,
-      alignment: Alignment.center,
+    BoxDecoration(
+      border: Border.all(
+        color: Colors.black,
+        width: 2.0,
+      )
     );
+    return Scaffold(
+      backgroundColor: CupertinoColors.white,
+      body: Padding(padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Divider(),
+                TextFormField(
+                    autofocus: true,
+                    keyboardType: TextInputType.text,
+                    style: new TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                      labelText: "Nome de usuário ou e-mail",
+                    )
+                ),
+                Divider(),
+                TextFormField(
+                  autofocus: true,
+                  keyboardType: TextInputType.text,
+                  style: new TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    labelStyle: TextStyle(color: Colors.grey),
+                    border: OutlineInputBorder(),
+                    floatingLabelBehavior: FloatingLabelBehavior.never, // O rótulo não se move
+                  ),
+                ),
+              ]
+            )
+          ),
+        ),
+      );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'login/tela1.dart';
+import 'package:telaestagio/login/tela1.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: Login(title: 'teste',),
+      home: Login(),
     );
   }
 }
