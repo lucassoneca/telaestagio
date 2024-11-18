cc908db055ee37d23f2b52dc64977ef0e0610886
Lucas Bezerra da Cruz <lucasbezerracrz@gmail.com>
2024-11-18 11:44:36 -0300

    Commit dos popups 2: implementei os popups aos ícones, coloquei uma margem em volta do mapa que será trabalhada em breve. Adicionei também a AppBar com todas as opções presentes na tela 'home.dart', que também estará presente na maioria das telas desse projeto. Alem disso, corrigi um erro de GlobalKey e o cancellable_tile_provider. Excluí do diretório o 'popup.dart', pois ainda não vi um motivo de uso para ele.

--- lib/Mapa/mapa.dart
+++ lib/Mapa/mapa.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:latlong2/latlong.dart';
+import '../Manutencao/manutencao.dart';
+import '../home/home.dart';
+import '../materiais/materiais.dart';
+import '../vistoria/vistoria.dart';
 import 'Pontos/pontos.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 
 class Mapa extends StatefulWidget {
   const Mapa({super.key});
@@ -13,8 +18,9 @@ class Mapa extends StatefulWidget {
 
 class _MapaState extends State<Mapa> {
   late Future<List<Pontos>> _pontosFuture;
-  final MapController _mapController = MapController();
   final PopupController _popupController = PopupController();
+  final dropValue = ValueNotifier('Manter paradas');
+  final dropOpcoes = ['Manter paradas', 'Mapa'];
 
   @override
   void initState() {
@@ -22,10 +28,69 @@ class _MapaState extends State<Mapa> {
     _pontosFuture = Pontos.procurarPontos();
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
+        leading: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Image.asset('images/onibus.png'), // Ícone do app
+        ),
+        actions: [
+          DropdownButtonHideUnderline(
+            child: ValueListenableBuilder(
+              valueListenable: dropValue,
+              builder: (BuildContext context, String value, _) {
+                return DropdownButton<String>(
+                  dropdownColor: Colors.blue,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  alignment: Alignment.center,
+                  icon: const Icon(Icons.arrow_drop_down, size: 35, color: Colors.white),
+                  value: (value.isEmpty) ? null : value,
+                  onChanged: (escolha) {
+                    dropValue.value = escolha.toString();
+                    if (escolha == 'Mapa') {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => Mapa()));
+                    } else if (escolha == 'Manter paradas') {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const Home()));
+                    }
+                  },
+                  items: dropOpcoes.map((opcao) {
+                    return DropdownMenuItem(
+                      value: opcao,
+                      child: Text(opcao),
+                    );
+                  }).toList(),
+                );
+              },
+            ),
+          ),
+          const SizedBox(width: 10),
+          _buildAppBarText('Manutenção', context),
+          const SizedBox(width: 20),
+          _buildAppBarText('Vistoria', context),
+          const SizedBox(width: 20),
+          _buildAppBarText('Materiais', context),
+          const SizedBox(width: 20),
+          TextButton(
+            onPressed: () { //botão 'deslogar' para voltar para a página de login
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Home()));
+            },
+            child: const Text(
+              'Voltar',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
       ),
       body: FutureBuilder<List<Pontos>>(
         future: _pontosFuture,
@@ -42,67 +107,153 @@ class _MapaState extends State<Mapa> {
             return const Center(child: Text('Nenhum ponto encontrado.'));
           }
 
-          final pontos = snapshot.data!; // Aqui estamos acessando a lista de pontos
+          final pontos = snapshot.data!;
 
-          return Stack(
-            children: [
-              PopupScope(
-                popupController: _popupController,
-                  child: FlutterMap(
-                    options: MapOptions(
-                      initialCenter: LatLng(-15.7801, -47.9292),
-                      initialZoom: 16,
-                    ),
-                    children: [
-                      TileLayer(
-                        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                        userAgentPackageName: 'com.example.app',
-                      ),
-                      MarkerClusterLayerWidget(
-                        options: MarkerClusterLayerOptions(
-                          maxClusterRadius: 250,
-                          markers: pontos.map((ponto) {
-                            final iconKey = GlobalKey();
-                            return Marker(
-                              key: iconKey,
-                              point: ponto.point,  // Supondo que 'Pontos' tenha um atributo 'point' do tipo LatLng
-                              width: 40,
-                              height: 40,
-                              child: Icon(Icons.pin_drop, color: Colors.blue,),
-                            );
-                          }).toList(),
-                          builder: (context, cluster) {
-                            return Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                color: Colors.blue,
-                              ),
-                              child: Center(
-                                child: Text(
-                                  '${cluster.length}',
-                                  style: const TextStyle(color: Colors.white, fontSize: 14),
-                                ),
-                              ),
-                            );
-                          },
-                         /* popupOptions: PopupOptions(
-                            popupController: _popupController,
-                            popupBuilder: (context, marker) {
-                              final condDftrans = markersProvider.encontrarCodDftransPorMarker(marker);
-
-                              if (condDftrans == null) {
-                                return const Text('Dados não disponíveis');
-                              }
-                            }
-                          )*/
+          return
+            Center(
+              child: Container(
+              height: MediaQuery.of(context).size.height * 0.8,
+              width: MediaQuery.of(context).size.width * 0.95,
+            child: PopupScope(
+            popupController: _popupController, // Adiciona PopupScope ao contexto
+            child: FlutterMap(
+              options: MapOptions(
+                initialCenter: LatLng(-15.7801, -47.9292),
+                initialZoom: 16,
+                onTap: (_, __) => _popupController.hideAllPopups(),
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                  tileProvider: CancellableNetworkTileProvider(),
+                ),
+                MarkerClusterLayerWidget(
+                  options: MarkerClusterLayerOptions(
+                    maxClusterRadius: 250,
+                    markers: pontos.map((ponto) {
+                      return Marker(
+                        point: ponto.point,
+                        width: 40,
+                        height: 40,
+                        child: const Icon(
+                          Icons.pin_drop,
+                          color: Colors.blue,
                         ),
+                      );
+                    }).toList(),
+                    builder: (context, cluster) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.blue,
+                        ),
+                        child: Center(
+                          child: Text(
+                            '${cluster.length}',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    popupOptions: PopupOptions(
+                      popupController: _popupController,
+                      popupBuilder: (context, marker) {
+                        final ponto = pontos.firstWhere(
+                              (p) => p.point == marker.point,
+                        );
+
+                        return Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min, // Ajusta a altura ao conteúdo
+                              children: [
+                                Text(
+                                  'Código: ${ponto.codDftrans}',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 4),
+                                Text('Sentido: (${ponto.sentido})'),
+                                const SizedBox(height: 4),
+                                Wrap(
+                                  spacing: 20.0, // Espaço horizontal entre os widgets
+                                  runSpacing: 7.0, // Espaço vertical entre as linhas, se necessário
+                                  children: [
+                                    InkWell(
+                                      child: Text(
+                                        'Editar',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          decoration: TextDecoration.underline,
+                                        ),
+                                      ),
+                                      onTap: () {
+                                        print('edit teste');
+                                      },
+                                    ),
+                                    InkWell(
+                                      child: Text(
+                                        'Desativar',
+                                        style: TextStyle(
+                                          color: Colors.red,
+                                          fontWeight: FontWeight.bold,
+                                          decoration: TextDecoration.underline,
+                                        ),
+                                      ),
+                                      onTap: () {
+                                        print('delet teste');
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
                       ),
-                    ],
-                  )
-              )
-            ],
-          );
-        },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    ),
+  );
+}
+
+  // Função auxiliar para adicionar textos de AppBar
+  Widget _buildAppBarText(String text, BuildContext context) {
+    //deixa os textos 'clicáveis' (linkados)
+    return InkWell(
+      onTap: () {
+        if (text == 'Manutenção') {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const manutencao()));
+          //link para a página de manutenção
+        } else if (text == 'Vistoria') {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const vistoria()));
+          //link para a página de vistoria
+        } else if (text == 'Materiais') {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const materiais()));
+          //link para a página de materiais
+        }
+      },
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 18,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     );
   }
